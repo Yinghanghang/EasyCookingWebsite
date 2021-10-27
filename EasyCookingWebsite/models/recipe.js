35fbd8eeb4cb0410e9c4bfef5c35af5324ea0f00
trungle01 <trung.le01@sjsu.edu>
2021-10-26 20:29:12 -0700

    Trung: - Add profile page - Change recipe model - Fix addrecipe.ejs, detailrecipe, routes/recipe.js to upload multi-images

--- EasyCookingWebsite/models/recipe.js
+++ EasyCookingWebsite/models/recipe.js
@@ -5,13 +5,16 @@ var recipeSchema = mongoose.Schema({
     category: { type: mongoose.Schema.Types.Array, require: false },
     prepareTime: { type: String, require: false },
     cookingTime: { type: String, require: false },
+    ingredient: { type: String, require: false },
     cookingSteps: { type: String, require: false },
-    image: { type: String, require: false, unique: false },
+    image: { type: Array, require: false, unique: false },
 /*    username: { type: String, require: false },  //show username of display  */
     userID: { type: mongoose.Schema.Types.ObjectId, require: false, unique: false },
     difficultLevel: { type: String, require: false },
+    like: { type: Number, require: false },
     createAt: { type: Date, default: Date.now }
 });
+
 
 var Recipe = mongoose.model("Recipe", recipeSchema);
 
